b021da8bd2af5ce2eca56e355527bcbfaba13d94
Gary Court <garycourt@gmail.com>
2010-08-31 18:21:30 -0600

    Added method and media type criteria testing.

--- tests/tests.js
+++ tests/tests.js
@@ -34,6 +34,71 @@ test("No Variants", function () {
 	equal(Negotiate.choose(variants, request).length, 0, "No variants, minimal request");
 });
 
+test("Method Criteria", function () {
+	var variants = [
+			{ id : 0, method : 'OPTIONS' },
+			{ id : 1, method : 'GET' },
+			{ id : 2, method : 'HEAD' },
+			{ id : 3, method : 'POST' },
+			{ id : 4, method : 'PUT' },
+			{ id : 5, method : 'DELETE' }
+		],
+		request = {
+			method : '',
+			headers : {}
+		};
+	
+	request.method = 'OPTIONS'; 
+	equal(Negotiate.choose(variants, request)[0].id, 0, "OPTIONS");
+	
+	request.method = 'GET'; 
+	equal(Negotiate.choose(variants, request)[0].id, 1, "GET");
+	
+	request.method = 'HEAD'; 
+	equal(Negotiate.choose(variants, request)[0].id, 2, "HEAD");
+	equal(Negotiate.choose(variants, request)[1].id, 1, "HEAD, GET");
+	
+	request.method = 'POST'; 
+	equal(Negotiate.choose(variants, request)[0].id, 3, "POST");
+	
+	request.method = 'PUT'; 
+	equal(Negotiate.choose(variants, request)[0].id, 4, "PUT");
+	
+	request.method = 'DELETE'; 
+	equal(Negotiate.choose(variants, request)[0].id, 5, "DELETE");
+});
+
+test("Media Type Criteria", function () {
+	var variants = [
+			{ id : 0, type : 'text/plain' },
+			{ id : 1, type : 'text/html' },
+			{ id : 2, type : 'application/xml' },
+			{ id : 3, type : 'application/xhtml+xml' },
+			{ id : 4, type : 'image/png' },
+			{ id : 5 }
+		],
+		request = {
+			method : 'GET',
+			headers : {}
+		};
+	
+	request.headers['accept'] = '*'; 
+	equal(Negotiate.choose(variants, request)[0].id, 0, "*");
+	
+	request.headers['accept'] = '*/*'; 
+	equal(Negotiate.choose(variants, request)[0].id, 0, "*/*");
+	
+	request.headers['accept'] = 'application/*'; 
+	equal(Negotiate.choose(variants, request)[0].id, 2, "application/*");
+	
+	request.headers['accept'] = 'application/xhtml+xml'; 
+	equal(Negotiate.choose(variants, request)[0].id, 3, "application/xhtml+xml");
+	
+	request.headers['accept'] = 'text/plain;q=0.4, image/* ; q = .8 ,application/xml,*/*;q=0.1'; 
+	equal(Negotiate.choose(variants, request)[0].id, 2, "Multiple[0]");
+	equal(Negotiate.choose(variants, request)[1].id, 4, "Multiple[1]");
+});
+
 test("Full Test", function () {
 	var variants = [
 		{
